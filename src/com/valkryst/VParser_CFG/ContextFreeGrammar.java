19e6d3b9547959bc773d7d48f41a08b880a4b4b7
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-06-23 15:23:42 -0300

    Adds null check to run function.

--- src/com/valkryst/VParser_CFG/ContextFreeGrammar.java
+++ src/com/valkryst/VParser_CFG/ContextFreeGrammar.java
@@ -69,6 +69,10 @@ public class ContextFreeGrammar {
      *        Result of the CFG.
      */
     public String run(String initialTransition) {
+        if (initialTransition == null) {
+            return "";
+        }
+
         boolean replacementOccurred = true;
 
         while (replacementOccurred) {
